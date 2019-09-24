9f800f388719673c93108bf536aced46e909ff23
ruslan <ruselsam89@gmail.com>
2019-09-24 22:09:15 +0300

    Create class Burgers

---
+++ src/main/java/Burgers.java
@@ -0,0 +1,11 @@
+public class Burgers {
+    String avatarUrl;
+    String name;
+    int price;
+    int weight;
+    String description;
+    int energyValue;
+    double proteins;
+    double fats;
+    double carbohydrates;
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Burgers oneBurger=new Burgers();
+        oneBurger.avatarUrl="https://s82079.cdn.ngenix.net/9VW3RQfY33AnJi7Yu38nKiHc.png?dw=230";
+        oneBurger.name="Cheeseburger";
+        oneBurger.price=69;
+        oneBurger.weight=167;
+        oneBurger.description="Пряный горчичный соус, кетчуп, сочные стрипсы в оригинальной панировке, лук, сыр Чеддер, огурцы на пшеничной булочке с кукурузной посыпкой.";
+        oneBurger.energyValue=237;
+        oneBurger.proteins=13.2;
+        oneBurger.fats=8.6;
+        oneBurger.carbohydrates=26.6;
+
+        Burgers twoBurger=new Burgers();
+        twoBurger.avatarUrl="https://s82079.cdn.ngenix.net/sRrSzWpf86c4hMMwCJ7aHBph.png?dw=230";
+        twoBurger.name="Longer BBQ";
+        twoBurger.price=50;
+        twoBurger.weight=107;
+        twoBurger.description="Сочная курочка, маринованные огурчики, лук и аппетитный соус барбекю! Спешите попробовать!";
+        twoBurger.energyValue=236;
+        twoBurger.proteins=9.7;
+        twoBurger.fats=7.1;
+        twoBurger.carbohydrates=33.3;
+    }
+}
